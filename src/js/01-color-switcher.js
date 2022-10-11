bd6685fbaeee9f70dd0ff5c387bdfe11c97e1bec
Vadym Chutur <falloutgreck@gmail.com>
2022-10-12 00:06:40 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,5 @@
 const DELAY_TIME = 1000;
+let timerId = null;
 
 const refs = {
   stopBtn: document.querySelector('[data-stop]'),
@@ -6,19 +7,29 @@ const refs = {
   bodyChangeColor: document.querySelector('body'),
 };
 
+disableBtn();
+
 refs.startBtn.addEventListener('click', startRandomSwitcher);
 refs.stopBtn.addEventListener('click', stopRandomSwitcher);
 
 function startRandomSwitcher() {
+  refs.startBtn.setAttribute('disabled', true);
+  refs.stopBtn.removeAttribute('disabled');
   timerId = setInterval(() => {
     refs.bodyChangeColor.style.backgroundColor = getRandomHexColor();
   }, DELAY_TIME);
 }
 
 function stopRandomSwitcher() {
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
   clearInterval(timerId);
 }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function disableBtn() {
+  refs.stopBtn.setAttribute('disabled', true);
 }
