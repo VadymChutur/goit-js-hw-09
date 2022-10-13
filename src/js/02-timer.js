793ab854b403b78904a94fe79c707e3940c16852
Vadym Chutur <falloutgreck@gmail.com>
2022-10-13 05:23:49 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,9 +4,15 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 let deadLine = null;
 
+const TIMER_DELAY = 1000;
+
 const refs = {
   timerRef: document.querySelector('.timer'),
   startBtnRef: document.querySelector('[data-start]'),
+  dayRef: document.querySelector('[data-days]'),
+  hoursRef: document.querySelector('[data-hours]'),
+  minutesRef: document.querySelector('[data-minutes]'),
+  secondsRef: document.querySelector('[data-seconds]'),
 };
 
 const options = {
@@ -18,30 +24,73 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
-    const numSelectedDates = selectedDates[0].getTime();
-    if (numSelectedDates < Date.now()) {
+    deadLine = selectedDates[0].getTime();
+    if (deadLine < Date.now()) {
       Notify.failure('The selected date is before todays date!', {
         closeButton: true,
         clickToClose: true,
+        backOverlay: true,
       });
+      refs.startBtnRef.setAttribute('disabled', false);
+    } else {
+      Notify.success('The selected date is greater than today!', {
+        clickToClose: true,
+        backOverlay: true,
+      });
+      refs.startBtnRef.removeAttribute('disabled');
     }
   },
 };
 
+refs.startBtnRef.addEventListener('click', startTimer);
+
 flatpickr('#datetime-picker', options);
+
+disableBtn();
 
 const timer = {
   intervalId: null,
   refs: {},
 
-  startTimer(rootSelector, deadLine) {
-    const deltaTime = deadLine - Date.now;
-    if (deltaTime <= 0) {
-    }
+  startTimer(rootSelector, date) {
+    this.getRefs(rootSelector);
+    this.intervalId = setInterval(() => {
+      const deltaTime = deadLine - Date.now();
+      if (deltaTime < 1000) {
+        clearInterval(this.intervalId);
+        Notify.success('The time is up (-_-)', {
+          clickToClose: true,
+          backOverlay: true,
+        });
+      }
+      const { days, hours, minutes, seconds } = convertMs(deltaTime);
+      this.refs.days.textContent = days;
+      this.refs.hours.textContent = hours;
+      this.refs.minutes.textContent = minutes;
+      this.refs.seconds.textContent = seconds;
+    }, TIMER_DELAY);
+  },
+
+  getRefs(rootSelector) {
+    this.refs.days = refs.dayRef;
+    this.refs.hours = refs.hoursRef;
+    this.refs.minutes = refs.minutesRef;
+    this.refs.seconds = refs.secondsRef;
   },
 };
 
-timer.startTimer(timerRef, deadLine);
+function startTimer() {
+  timer.startTimer(refs, deadLine);
+  refs.startBtnRef.setAttribute('disabled', false);
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+function padDays(value) {
+  return String(value).padStart(3, '0');
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -51,17 +100,17 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = padDays(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = pad(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function disableBtn() {
+  refs.startBtnRef.setAttribute('disabled', true);
+}
